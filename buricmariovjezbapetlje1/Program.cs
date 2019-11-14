ccaaaf1e1d5f4c3ba47e2933f93e73dd2ec8fa9b
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2019-11-14 11:33:39 +0100

    Add files via upload

---
+++ buricmariovjezbapetlje1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricmariovjezbapetlje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocjena;
+            float prosjek;
+            int zbrojOcjena = 0;
+            float brojOcjena = 0;
+            
+
+            do
+            {
+
+
+                Console.WriteLine("upisite ocjenu:");
+                ocjena = Convert.ToInt16(Console.ReadLine());
+                brojOcjena++;
+                if (ocjena == 1)
+                {
+                    prosjek = 1;
+                    break;
+                }
+                 
+                else if (ocjena > 5 || ocjena < 0)
+
+                {
+                    Console.WriteLine("Pogresan unos,unesite ponovno.");
+                }
+                else if (ocjena != 0)
+                {
+                    brojOcjena++;
+                    zbrojOcjena += ocjena;
+                    
+                }
+                prosjek = zbrojOcjena / brojOcjena;
+            }
+
+            while (ocjena != 0);
+            Console.WriteLine("Prosjek ocijena je " + prosjek);
+            Console.ReadKey();
+
+
+            }
+        
+
+    }
+    }
+
